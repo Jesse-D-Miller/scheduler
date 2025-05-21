c45eaacccd054e5cc90990a164092b8c6441fd48
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-21 15:39:06 -0700

    continueing to follow instructions to implement index.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [history, setHistory] = useState([initial]); 
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode){
-    setHistory(prev => [...prev, newMode])
+  function transition(mode, replace = false) {
+    setHistory(prev => (
+      replace
+        ? [...prev.slice(0, prev.length - 1), mode]
+        : [...prev, mode]
+    ));
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    if (history.length !== 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    } else {
+      return history;
+    }
   }
 
-  return { mode: history[history.length -1], transition, back };
+  return { mode: history[history.length - 1], transition, back };
 }
